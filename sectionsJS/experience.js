ba204018e18d4e1f5671f5345490ee3b5cd46f49
VIMAL RAJ N <173754633+Vimal-AFK@users.noreply.github.com>
2025-03-29 22:36:34 +0530

    Update experience.js

--- sectionsJS/experience.js
+++ sectionsJS/experience.js
@@ -5,7 +5,7 @@ function experience() {
     <div class="experience-container">
         <h2 class="experience-title">My Experience</h2>
         <div class="experience-item">
-            <img src="../images/experienceImage/aptitudeIMG1.jpg" alt="Experience 3" class="experience-image">
+            <img src="images/experienceImage/aptitudeIMG1.jpg" alt="Experience 3" class="experience-image">
             <p class="experience-description">Worked on a college placement testing system as part of a team, deploying it in a local system to ensure malpractice-free testing.</p>
         </div>
     </div>

7ba9be55d709cfa8939cbdabdc6e1cb71a80b298
VIMAL RAJ N <173754633+Vimal-AFK@users.noreply.github.com>
2025-03-30 21:38:18 +0530

    Update resume.js

--- sectionsJS/resume.js
+++ sectionsJS/resume.js
@@ -3,8 +3,11 @@ function resume() {
 
   let resumeHTML = `
     <div class="resume-container">
-        <h2 class="resume-title">My Resume</h2>
-        <a href="https://drive.google.com/file/d/1hMqY75BYvclgCiz86GPqSQj6ckY3aTl9/view?usp=sharing" class="resume-download">Download Resume</a>
+        <h2 class="resume-title">📄 My Resume</h2>
+        <a href="https://drive.google.com/file/d/1hMqY75BYvclgCiz86GPqSQj6ckY3aTl9/view?usp=sharing" 
+           class="resume-download" target="_blank" rel="noopener noreferrer">
+           🔗 Download Resume
+        </a>
     </div>
   `;
 
